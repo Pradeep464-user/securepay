52e377d03764065284ba881d3dbbeeb2dd196318
Pradeep R <pradeepraghunathan16@gmail.com>
2026-09-12 21:59:59 +0530

    Connect Backend to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 const PORT = 5000;
+
+connectDB();
 
 app.get("/api/test", (req, res) => {
     res.json({ message: "SecurePay backend is working!" });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:5000`);
 });
