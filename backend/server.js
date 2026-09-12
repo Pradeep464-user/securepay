19f5bb4868cf3097fa9e6b2453f8aef18ba674c9
Pradeep R <pradeepraghunathan16@gmail.com>
2026-09-12 22:23:58 +0530

    Add user registration backend

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,22 @@
+require("dotenv").config();
+
 const express = require("express");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 const PORT = 5000;
 
 connectDB();
 
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
 app.get("/api/test", (req, res) => {
     res.json({ message: "SecurePay backend is working!" });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:5000`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
